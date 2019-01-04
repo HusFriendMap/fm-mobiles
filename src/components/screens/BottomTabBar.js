fc9dc0c093d843d138a209327160fe98a743b775
LightKnight3r <hoangminh135674@gmail.com>
2019-01-04 22:45:35 +0700

    Thư viện Icons - Thay đổi Bottom bar

--- src/components/screens/BottomTabBar.js
+++ src/components/screens/BottomTabBar.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import {
   StyleSheet,
@@ -5,18 +6,29 @@ import {
   View,
   TouchableOpacity,
 } from 'react-native';
-import {Icon} from 'native-base';
+import Icon from 'react-native-vector-icons/AntDesign';
+import * as Animatable from 'react-native-animatable';
+var Themes = require('../../Themes');
+import { connect } from 'react-redux';
+var Define = require('../../Define');
+var Themes = require('../../Themes');
 
 class BottomTabBar extends React.Component {
-  icons = [];
 
   constructor(props) {
     super(props);
+    this.state = _.merge(this.state,
+    {
+      number:0,
+    })
     this.icons = [];
     this.names = [];
+    this.iconLabels = ['home','hearto', 'message1', 'user'];
+
   }
 
   componentDidMount() {
+    this.setAnimationValue({value: 0})
     this._listener = this.props.scrollValue.addListener(this.setAnimationValue.bind(this));
   }
 
@@ -29,8 +41,9 @@ class BottomTabBar extends React.Component {
         },
       });
     });
+
     this.names.forEach((name, i) => {
-      const progress = (value - i >= 0 && value - i <= 1) ? value - i : 1;
+      const progress = (Math.abs(value - i) >= 0 && Math.abs(value - i) <= 1) ? Math.abs(value - i) : 1;
       name.setNativeProps({
         style: {
           color: this.iconColor(progress),
@@ -41,87 +54,32 @@ class BottomTabBar extends React.Component {
 
   //color between rgb(59,89,152) and rgb(204,204,204)
   iconColor(progress) {
-    const red = 59 + (204 - 59) * progress;
-    const green = 89 + (204 - 89) * progress;
-    const blue = 152 + (204 - 152) * progress;
+    const red = 238 -  238 * progress;
+    const green = 127  - 127 * progress;
+    const blue = 25  - 25 * progress;
     return `rgb(${red}, ${green}, ${blue})`;
   }
-  renderTextUnderIcon(index) {
-    switch (index) {
-      case 0:
-        return(
-          <View>
-            <Text
-              color={this.props.activeTab === index ? 'rgb(59,89,152)' : 'rgb(204,204,204)'}
-              ref={(name) => { this.names[index] = name; }}
-              style={{fontSize: 11}}>
-              Trang chủ</Text>
-          </View>
-        )
-      break;
-      case 1:
-        return(
-          <View>
-          <Text
-            color={this.props.activeTab === index ? 'rgb(59,89,152)' : 'rgb(204,204,204)'}
-            ref={(name) => { this.names[index] = name; }}
-            style={{fontSize: 11}}>
-            Đơn hàng</Text>
-          </View>
-        )
-      break;
-      case 2:
-        return(
-          <View>
-            <Text
-              color={this.props.activeTab === index ? 'rgb(59,89,152)' : 'rgb(204,204,204)'}
-              ref={(name) => { this.names[index] = name; }}
-              style={{fontSize: 11}}>
-              chatboxes</Text>
-          </View>
-        )
-      break;
-      case 3:
-        return(
-          <View>
-          <Text
-            color={this.props.activeTab === index ? 'rgb(59,89,152)' : 'rgb(204,204,204)'}
-            ref={(name) => { this.names[index] = name; }}
-            style={{fontSize: 11}}>
-            notifications</Text>
-          </View>
-        )
-      break;
-      case 4:
-        return(
-          <View>
-          <Text
-            color={this.props.activeTab === index ? 'rgb(59,89,152)' : 'rgb(204,204,204)'}
-            ref={(name) => { this.names[index] = name; }}
-            style={{fontSize: 11}}>
-            list</Text>
-          </View>
-        )
-      break;
-      default:
-        return null;
-    }
-
-  }
   render() {
-    return <View style={[styles.tabs, this.props.style, ]}>
+    return <View style={[styles.tabs, this.props.style, ], {flexDirection:'row'}}>
       {this.props.tabs.map((tab, i) => {
         return <TouchableOpacity key={tab} onPress={() => this.props.goToPage(i)} style={styles.tab}>
           <View
             style={{flexDirection:'column', alignSelf:'center', alignItems:'center', justifyContent:'center'}}
           >
-            <Icon
-              name={tab}
-              size={25}
-              color={this.props.activeTab === i ? 'rgb(59,89,152)' : 'rgb(204,204,204)'}
-              ref={(icon) => { this.icons[i] = icon; }}
-            />
-            {this.renderTextUnderIcon(i)}
+            <View>
+              <Icon
+                name={this.iconLabels[i]}
+                size={23}
+                style={{'backgroundColor': 'transparent', color: '#fff', marginBottom: -5}}
+                ref={(icon) => { this.icons[i] = icon; }}
+              />
+            </View>
+            <View>
+              <Text
+                ref={(name) => { this.names[i] = name; }}
+                style={{fontSize: 14, backgroundColor: 'transparent', color: '#fff'}}>
+                {tab}</Text>
+            </View>
           </View>
 
         </TouchableOpacity>;
@@ -135,14 +93,15 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    paddingBottom: 10,
+    paddingBottom: 3,
   },
   tabs: {
-    height: 50,
-    flexDirection: 'row',
-    paddingTop: 5,
-    backgroundColor:'#edfffa',
-    elevation:3
+    height: 45,
+    elevation:1,
+    shadowColor:'#000000',
+    shadowOpacity:0.5,
+    shadowOffset:{height:-1, width:0},
+    borderColor:'#000'
   },
 });
 
